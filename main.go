c22ce86767c7e5e5f51ad6a999a6d89a41cb07e0
Alena <m1nima02@yandex.ru>
2024-05-23 19:37:07 +0300

    добавлен набросок решения

--- main.go
+++ main.go
@@ -2,31 +2,53 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type Cache interface {
 	Get(k string) (string, bool)
 	Set(k, v string)
+	Cleanup()
 }
 
 var _ Cache = (*cacheImpl)(nil)
 
 // Доработает конструктор и методы кеша, так чтобы они соответствовали интерфейсу Cache
 func newCacheImpl() *cacheImpl {
-	return &cacheImpl{}
+	return &cacheImpl{
+		    data:       make(map[string]string),
+		    expiration: make(map[string]time.Time),
+	}
 }
 
 type cacheImpl struct {
+	data       map[string]string
+	expiration map[string]time.Time
 }
 
 func (c *cacheImpl) Get(k string) (string, bool) {
 	// TODO implement me
-	return "", false
+	c.Cleanup() 
+	v, ok := c.data[k]
+	return v, ok
 }
 
 func (c *cacheImpl) Set(k, v string) {
 	// TODO implement me
+	c.data[k] = v
+	c.expiration[k] = time.Now().Add(5 * time.Minute)
 }
+
+func (c *cacheImpl) Cleanup() {
+	now := time.Now()
+	for k, exp := range c.expiration {
+		if now.After(exp) {
+			delete(c.data, k)
+			delete(c.expiration, k)
+		}
+	}
+}
+
 
 func newDbImpl(cache Cache) *dbImpl {
 	return &dbImpl{cache: cache, dbs: map[string]string{"hello": "world", "test": "test"}}
@@ -50,6 +72,9 @@ func (d *dbImpl) Get(k string) (string, bool) {
 func main() {
 	c := newCacheImpl()
 	db := newDbImpl(c)
+	db.cache.Set("key1", "value1")
+	db.cache.Set("key2", "value2")
+	time.Sleep(6 * time.Minute)
 	fmt.Println(db.Get("test"))
 	fmt.Println(db.Get("hello"))
 }
